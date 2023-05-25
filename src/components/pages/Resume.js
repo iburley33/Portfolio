4ca0ff711c28eafc20343300b3cd2cde52c6ca48
iburley33 <iburley33@gmail.com>
2023-05-24 21:10:40 -0400

    complete

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
       <h3>Client Facing</h3>
       <p> HTML, CSS, JS, React, ApolloClient, jQuery, Bootstrap, Bulma.</p>
       <h3>Back End</h3>
-      <p>API, Node.js, Express, ApolloServer,MySQL, Sequelize, GraphQL, Mongoose, MongoDB.</p>
+      <p>API, Node.js, Express, ApolloServer, MySQL, Sequelize, GraphQL, Mongoose, MongoDB.</p>
       </div>
       <div class='div1'>
         <h1>Personal Pursuits</h1>
@@ -16,8 +16,8 @@ export default function Resume() {
           I'm currently a member of multiple groups who simply enjoy building projects together. I am always looking for new opportunities to polish my full stack development skills. I have a mentor who I work with regularly to continue to broaden my knowledge base. I am always keen to dive head first into problems or bugs that I don't understand and treat them like a new rubiks cube. Not a single day passes that I'm not building something.
         </p>
         <h1>Life Before Coding: A life of leadership</h1>
-        <p>Before coding I was a regional manager for a national health club corporation for over a decade. I was directly responsible for locations ranging from Virginia all the way to Rhode Island. My life was remarkably fast paced and much of my responsibilities were regularly handled from airports and planes due to living life on the road 2 weeks out of the month.  </p>
-        <p>It was hard work but I wouldn't trade that experience for anything. I am very thankful for the opportunities I have been given and the life they have provided me but over time I realized that a management career did not allow me enough opportunities to create something new from nothing.</p>
+        <p>Before coding I was a regional manager for a national health club corporation for over a decade. I was directly responsible for locations ranging from Virginia all the way to Rhode Island. My life was remarkably fast paced and many of my responsibilities were regularly handled from airports and planes due to living life on the road 2 weeks out of the month.  </p>
+        <p>It was hard work but I wouldn't trade that experience for anything. I am very thankful for the opportunities I have been given and the life they have provided me but over time I realized that a management career did not allow me enough opportunities to create something new from nothing. I need to be building. I need to be creating. I need to be solving.</p>
       </div>
     </div>
   );

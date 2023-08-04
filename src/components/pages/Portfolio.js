ea87a3b796ccff488617bc57b459daf110296fbd
iburley33 <iburley33@gmail.com>
2023-08-04 15:45:56 -0400

    Added 3rd project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Project from '../Project';
 import velocity from '../../imgs/velocity.png';
-
-import eatball from '../../imgs/eatball.png'
+import BH from '../../imgs/BH SS.png';
+import eatball from '../../imgs/eatball.png';
 
 export default function Portfolio() {
   const propsArray = [
     {
-      name: "Velocity",
-      desc: "Your one stop shop for all things cycling. Find your nearest cycling route and current weather conditions in 1 quick search.",
-      link: 'https://young-river-94596.herokuapp.com/login',
-      image: velocity
+      name: "Burley Homr",
+      desc: "Full service E-Commerce app for home design and furniture shopping",
+      link: 'https://main.d1nlbx8b203g2u.amplifyapp.com/',
+      image: BH
     },
     {
       name: "The Magic Eat Ball!",
